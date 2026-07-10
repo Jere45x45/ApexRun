ef8b50d949423bb73bf13d8d8b1b1a2e9d7c72ce
Jere <a@gamil.com>
2026-07-10 15:35:32 -0300

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float turnSpeed = 100f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * move * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * turn * turnSpeed * Time.deltaTime);
+    }
+}
