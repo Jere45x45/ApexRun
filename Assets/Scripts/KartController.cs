e96e5f17d6053d70789180aefcbf1c4ed6a4956a
Jere <a@gamil.com>
2026-07-10 17:58:57 -0300

    Test2

---
+++ Assets/Scripts/KartController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class KartController : MonoBehaviour
+{
+    public float acceleration = 5000f;
+    public float maxSpeed = 20f;
+
+    public float steeringForce = 1000f;
+
+    private Rigidbody rb;
+
+    private float move;
+    private float turn;
+
+    private float speed;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        move = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        speed = rb.velocity.magnitude;
+
+        rb.AddForce(transform.forward * move * acceleration);
+
+        rb.AddTorque(Vector3.up * turn * steeringForce * speed * Time.fixedDeltaTime);
+
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
