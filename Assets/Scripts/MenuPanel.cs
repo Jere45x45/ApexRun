53e0b155eaf1ed8511539aed406d1beca4187a7e
Jere45x45 <jerebeyo@gmail.com>
2026-08-05 14:30:40 -0300

    Base de la UI

---
+++ Assets/Scripts/MenuPanel.cs
@@ -0,0 +1,7 @@
+﻿public enum MenuPanel
+{
+    Main,
+    Multiplayer,
+    Settings,
+    AIMechanic
+}
---
+++ Assets/Scripts/UIPanel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class UIPanel
+{
+    public MenuPanel type;
+
+    public GameObject panel;
+}
