8a582cac87264296635722ef1cb8e4505d632339
Julia-Wag <juli.w.081009@gmail.com>
2026-08-06 10:33:24 -0300

    ok

---
+++ Assets/Scripts/BotController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour
+{
+     public KartBehaviour kart;
+
+    void Update()
+    {
+        kart.SetInputs(1f, 0f, false);
+    }
+}
