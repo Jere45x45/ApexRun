dde0068aac31a6b2e99517f21b26ba0c30496465
Jere <a@gamil.com>
2026-08-05 23:09:01 -0300

    Siguiendo con la UI

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField] private MenuManager menuManager;
+    [SerializeField] private MenuPanel targetPanel;
+
+    public void OpenPanel()
+    {
+        menuManager.Show(targetPanel);
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,4 +5,21 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField]
     private List<UIPanel> panels = new List<UIPanel>();
+
+    [SerializeField]
+    private MenuPanel startPanel = MenuPanel.Main;
+
+    private void Start()
+    {
+        Show(startPanel);
+    }
+
+    public void Show(MenuPanel panelType)
+    {
+        foreach (UIPanel panel in panels)
+        {
+            bool active = panel.type == panelType;
+            panel.panel.SetActive(active);
+        }
+    }
 }
