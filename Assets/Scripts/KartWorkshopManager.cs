a37ccec8f44abced583365d1bdfbdf0b9ed44e92
BEYOGLONIAN JEREMÍAS AGUSTÍN <49839297@ort>
2026-08-10 10:40:51 -0300

    Modificando la arquitectura del proyecto con respecto al tema garage / tallerdel

---
+++ Assets/Scripts/KartWorkshopManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class KartWorkshopManager : MonoBehaviour
+{
+    [SerializeField] private KartBehavior kartBehavior;
+
+    public void InstallPart(KartPart part)
+    {
+        if (part == null)
+            return;
+
+        part.Install(kartBehavior.Kart.Configuration);
+
+        kartBehavior.RefreshKart();
+    }
+}
